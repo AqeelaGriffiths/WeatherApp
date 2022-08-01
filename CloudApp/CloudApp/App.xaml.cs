f4b9a572902713fdf6fec1d796c5beecece53e1e
AqeelaGriffiths <102726835+AqeelaGriffiths@users.noreply.github.com>
2022-08-01 09:57:45 +0200

    Add project files.

---
+++ CloudApp/CloudApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace WeatherApp
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new NavigationPage(new WeatherPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ CloudApp/CloudApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CloudApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly object getWeatherBtn;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Title = "Sample Weather App";  
+            getWeatherBtn.Clicked += GetWeatherBtn_Clicked;
+            this.BindingContext = new Weather();
+        }
+       
+        public async void GetWeatherBtn_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(zipCodeEntry.Text))
+            {
+                Weather weather = await Core.GetWeather(zipCodeEntry.Text);
+                if (weather != null)
+                {
+                    this.BindingContext = weather;
+                    getWeatherBtn.Text = "Search Again";
+                }
+            }
+        }
+    }
+}
